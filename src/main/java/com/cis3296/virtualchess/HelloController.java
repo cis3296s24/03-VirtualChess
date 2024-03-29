71d968d64985d0ef4a0f57b26602463cfe4d6375
Nicholas Rucinski <tur45021@temple.edu>
2024-03-29 13:59:01 -0400

    created a blank project to work off of

---
+++ src/main/java/com/cis3296/virtualchess/HelloController.java
@@ -0,0 +1,14 @@
+package com.cis3296.virtualchess;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
