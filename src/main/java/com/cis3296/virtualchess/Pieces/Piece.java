d21119c3d6339a112c609f4db2d4453fecabfec4
Matt Gibson <mattgibson333@gmail.com>
2024-04-03 19:58:53 -0400

    Created the Pieces package and the superclass Piece for which each piece type will be a child of. Also added in some final variables into main for generic ease of alteration later if need be

---
+++ src/main/java/com/cis3296/virtualchess/Pieces/Piece.java
@@ -0,0 +1,40 @@
+package com.cis3296.virtualchess.Pieces;
+
+import com.cis3296.virtualchess.Board;
+
+public class Piece {
+    public int x, y; // Pixel coordinates for the pieces
+    public int col, row, prevCol, prevRow; // We want to track previous location in addition to current location
+
+    /**
+     * Constructor method for pieces
+     * @param col is the column number of the piece
+     * @param row is the row number of the piece
+     */
+    public Piece(int col, int row) {
+        this.col = col;
+        this.row = row;
+        x = getX(col);
+        y = getY(row);
+        prevCol = col;
+        prevRow = row;
+    }
+
+    /**
+     * Getter method for the X value of a piece
+     * @param col is the column number
+     * @returns col * SQUARE_SIZE. This is the X-coordinate pixel location of the piece
+     */
+    public int getX(int col) {
+        return col * Board.SQUARE_SIZE;
+    }
+
+    /**
+     * Getter method for the Y value of a piece
+     * @param row is the row number
+     * @returns row * SQUARE_SIZE. This is the Y-coordinate pixel location of the piece
+     */
+    public int getY(int row) {
+        return row * Board.SQUARE_SIZE;
+    }
+}
