4eae69878006552bcfccdb507ec877d85f3870b2
Aidan Cuff <aidan.cuff@temple.edu>
2024-04-05 01:49:00 -0400

    Added menu screen, ability to switch to game, and css

--- src/main/java/com/cis3296/virtualchess/Controller.java
+++ src/main/java/com/cis3296/virtualchess/Controller.java
@@ -1,25 +1,31 @@
 package com.cis3296.virtualchess;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    GridPane chessBoard;
+    GridPane chessBoard = new GridPane();
 
     /**
      * Creates the chess board
      */
-    public void initialize(){
+    public void initialize() throws IOException {
         Game game = new Game(chessBoard);
     }
+
 }
+
