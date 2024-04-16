84826ab985c90e3b2265d341a67439b4fa9ab7d3
Nicholas Rucinski <tur45021@temple.edu>
2024-04-16 11:09:17 -0400

    added a player table

--- src/main/java/com/cis3296/virtualchess/Database.java
+++ src/main/java/com/cis3296/virtualchess/Database.java
@@ -2,20 +2,34 @@ package com.cis3296.virtualchess;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Database {
 
     void databaseTest(){
         Connection c = null;
+        Statement stmt = null;
 
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " PLAYER1           TEXT    NOT NULL, " +
+                    " PLAYER1RESULT        CHAR(50), " +
+                    " PLAYER2           TEXT    NOT NULL, " +
+                    " PLAYER2RESULT        CHAR(50))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
+        System.out.println("Table created successfully");
     }
 
 }
