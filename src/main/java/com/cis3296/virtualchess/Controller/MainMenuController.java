10e59a13985e01a61b49702ea79b2c3881c59388
Toker Ahmed <tup19966@temple.edu>
2024-04-20 02:41:54 -0400

    Added button Learn Chess and setup the tutorial screen page

--- src/main/java/com/cis3296/virtualchess/Controller/MainMenuController.java
+++ src/main/java/com/cis3296/virtualchess/Controller/MainMenuController.java
@@ -59,4 +59,14 @@ public class MainMenuController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchToTutorial(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/cis3296/virtualchess/Tutorial Screen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
---
+++ src/main/java/com/cis3296/virtualchess/Controller/TutorialScreenController.java
@@ -0,0 +1,52 @@
+package com.cis3296.virtualchess.Controller;
+
+import com.cis3296.virtualchess.Entities.Player;
+import com.cis3296.virtualchess.Systems.TurnSystem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class TutorialScreenController {
+
+    @FXML
+    public AnchorPane menu;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    public void switchToSelection(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/cis3296/virtualchess/Tutorial Screen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/com/cis3296/virtualchess/menuStyle.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToMainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/cis3296/virtualchess/mainmenu.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/cis3296/virtualchess/menuStyle.css")).toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
