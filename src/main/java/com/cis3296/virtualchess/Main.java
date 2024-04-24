f150ae7bda29c805c3f5f6af3ba42ae492b272ea
Nicholas Rucinski <tur45021@temple.edu>
2024-04-24 08:50:15 -0400

    commented main

--- src/main/java/com/cis3296/virtualchess/Main.java
+++ src/main/java/com/cis3296/virtualchess/Main.java
@@ -3,6 +3,8 @@ package com.cis3296.virtualchess;
 public class Main{
 
     public static void main(String[] args) {
+        // Starts the chest app
+        // Had to do it this way to get the JAR easily
         ChessApp.main(args);
     }
 }
